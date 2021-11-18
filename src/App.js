f22da563be48b1ec0f7776977b82d72e71270426
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2021-11-18 10:30:53 +0100

    stateful

--- src/App.js
+++ src/App.js
@@ -1,73 +1,18 @@
-import React from 'react'
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/MahdiBehi">MahdiBehi</a>
-    </div>
-  )
-}
+import React, { useState } from 'react'
 
-const Hello = ({name, age}) => {
-  
-  /*
-  const name = props.name
-  const age = props.age
-  /*
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }*/
-
-  const bornYear = () => new Date().getFullYear() - age
-  return (
-    <div>
-    <p>
-  
-    Hello {name}, you are {age} years old
-    
-    </p>
-    <p>So you were born in {bornYear()}</p>
-    </div>
-  )
-}
-
-/*
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
 
-  const now = new Date()
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
 
-  const a = 10
-  
-  const b = 20
+  console.log('rendering...', counter)
 
-  const name = 'Peter'
-
-  const age = 10
-
-  console.log('Hello from app component')
   return (
-  
-    <div>
-      <p>Greetings</p>
-      <p>
-        {a} plus {b} is {a+b}
+    <div>{counter}</div>
+  )
+}
 
-        now is {}
-        <Hello name="Maya" age={26 + 10} />
-        <Hello name={name} age={age} />
-        <Footer />
-
-      </p>
-    </div>
-
-    )
-  }
-  */
-  const App = (props) => {
-    const {counter} = props
-    return (
-      <div><h1>{counter}</h1></div>
-    )
-  }
-
-export default App
+export default App  
