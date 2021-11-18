0dcf1996fbcb59b1172b74b32ef2a9f93930d53c
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2021-11-18 10:08:57 +0100

    part 1 fullstack course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+const Footer = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      greeting app created by <a href="https://github.com/MahdiBehi">MahdiBehi</a>
     </div>
-  );
+  )
 }
 
-export default App;
+const Hello = ({name, age}) => {
+  
+  /*
+  const name = props.name
+  const age = props.age
+  /*
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }*/
+
+  const bornYear = () => new Date().getFullYear() - age
+  return (
+    <div>
+    <p>
+  
+    Hello {name}, you are {age} years old
+    
+    </p>
+    <p>So you were born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const now = new Date()
+
+  const a = 10
+  
+  const b = 20
+
+  const name = 'Peter'
+
+  const age = 10
+
+  console.log('Hello from app component')
+  return (
+  
+    <div>
+      <p>Greetings</p>
+      <p>
+        {a} plus {b} is {a+b}
+
+        now is {}
+        <Hello name="Maya" age={26 + 10} />
+        <Hello name={name} age={age} />
+        <Footer />
+
+      </p>
+    </div>
+
+    )
+  }
+
+export default App
